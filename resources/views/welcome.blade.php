47c1caae64d20573c4c804a1e991d18fdbbd44f4
mj23-coder <calupcupan.marcjustin.bsit@gmail.com>
2024-09-19 15:36:59 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+    <title>Veterinary</title>
+    </head>
+    <body>
+        <Section>
+         <img class="background" src="{{ asset('images/about.jpg') }}" alt="">
+            <div class=nav>
+             <img class="nav-pic" src="{{ asset('images/logo.png') }}" alt="">
+            </div>
+        </section>
+    </body>
+</html>
